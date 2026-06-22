319bd8cef8b3961606b09796190d818562672dc2
Nishant Kumar <nishant.kumar83358@gmail.com>
2026-06-22 21:36:33 +0530

    Fixed render port binding

--- backend/app.py
+++ backend/app.py
@@ -1,17 +1,23 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_cors import CORS
 from models import db
 from routes import register_routes
-from flask_cors import CORS
+import os
+
 
 app = Flask(__name__)
 
 CORS(app)
 
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ranking.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+
 db.init_app(app)
+
 register_routes(app)
+
 
 with app.app_context():
     db.create_all()
@@ -19,10 +25,23 @@ with app.app_context():
 
 @app.route('/')
 def home():
-    return {
+    return jsonify({
         "message": "Backend Running"
-    }
+    })
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+
+    port = int(
+        os.environ.get('PORT', 5000)
+    )
+
+    app.run(
+
+        host='0.0.0.0',
+
+        port=port,
+
+        debug=False
+
+    )
